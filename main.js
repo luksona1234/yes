fe0abf7221dd2e5dc064eec215c22e63fa97ab73
Luka <lukakhachvani0311@gmail.com>
2023-03-12 22:25:51 +0400

    yes

---
+++ main.js
@@ -0,0 +1,83 @@
+let classes = document.querySelector('.classes');
+let c1 = document.querySelector('.c1')
+let c2 = document.querySelector('.c2')
+let c3 = document.querySelector('.c3')
+let c4 = document.querySelector('.c4')
+let c5 = document.querySelector('.c5')
+let c6 = document.querySelector('.c6')
+let c7 = document.querySelector('.c7')
+let c8 = document.querySelector('.c8')
+
+c1.addEventListener('mouseover', ()=> {
+    c1.style.color = 'blue'
+    
+})
+c1.addEventListener('mouseout', ()=> {
+    if(c1.style.color = 'blue'){
+        c1.style.color = 'black'
+    }
+})
+c2.addEventListener('mouseover', ()=> {
+    c2.style.color = 'blue'
+    
+})
+c2.addEventListener('mouseout', ()=> {
+    if(c2.style.color = 'blue'){
+        c2.style.color = 'black'
+    }
+})
+c3.addEventListener('mouseover', ()=> {
+    c3.style.color = 'blue'
+    
+})
+c3.addEventListener('mouseout', ()=> {
+    if(c3.style.color = 'blue'){
+        c3.style.color = 'black'
+    }
+})
+c4.addEventListener('mouseover', ()=> {
+    c4.style.color = 'blue'
+    
+})
+c4.addEventListener('mouseout', ()=> {
+    if(c4.style.color = 'blue'){
+        c4.style.color = 'black'
+    }
+})
+c5.addEventListener('mouseover', ()=> {
+    c5.style.color = 'blue'
+    
+})
+c5.addEventListener('mouseout', ()=> {
+    if(c5.style.color = 'blue'){
+        c5.style.color = 'black'
+    }
+})
+c6.addEventListener('mouseover', ()=> {
+    c6.style.color = 'blue'
+    
+})
+c6.addEventListener('mouseout', ()=> {
+    if(c6.style.color = 'blue'){
+        c6.style.color = 'black'
+    }
+})
+c7.addEventListener('mouseover', ()=> {
+    c7.style.color = 'blue'
+    
+})
+c7.addEventListener('mouseout', ()=> {
+    if(c7.style.color = 'blue'){
+        c7.style.color = 'black'
+    }
+})
+c8.addEventListener('mouseover', ()=> {
+    c8.style.color = 'blue'
+    
+})
+c8.addEventListener('mouseout', ()=> {
+    if(c8.style.color = 'blue'){
+        c8.style.color = 'black'
+    }
+})
+
